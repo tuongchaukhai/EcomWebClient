a264f83b71dbd4f35cc5f475106850853ee52491
tuongchaukhai <tuong.chaukhai@gmail.com>
2023-05-01 22:47:29 +0700

    Created resuable-table component

---
+++ src/app/shared/reusable-table/reusable-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reusable-table',
+  templateUrl: './reusable-table.component.html',
+  styleUrls: ['./reusable-table.component.scss']
+})
+export class ReusableTableComponent {
+
+}
