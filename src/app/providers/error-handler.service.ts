4f1cc26bb188c163db2ddb56b33a3081de77636b
tuongchaukhai <tuong.chaukhai@gmail.com>
2023-05-01 22:46:35 +0700

    Added error handler service

---
+++ src/app/providers/error-handler.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorHandlerService {
+
+  constructor() { }
+
+  handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server
+      // if (error.status == 401) {
+      //   errorMessage = `You didn't log in`;
+      //   // this.authService.logOut();
+      //   this.router.navigate(['/login']);
+      // }
+      // else if (error.status == 400) {
+      //   errorMessage = `You didn't have permission to`;
+      // }
+      // else {
+      errorMessage = `Error Code: ${error.status}`;
+    }
+    console.error(errorMessage);
+    alert(errorMessage);
+    return throwError(errorMessage);
+  }
+}
